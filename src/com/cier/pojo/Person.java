138404ea41e8897fccf2d3828c4194cbf8f6c82b
liuenci <154910381@qq.com>
2018-07-12 16:10:04 +0800

    序列化id

--- src/com/cier/pojo/Person.java
+++ src/com/cier/pojo/Person.java
@@ -2,10 +2,12 @@ package com.cier.pojo;
 
 import java.io.Serializable;
 
-public class Person implements Serializable{
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = -167829758596052490L;
     private String name;
     private int age;
-
+    private String gender;
     public Person() {
     }
 
