b7cf5badb6b966e189a995306cc62ba901fe5019
liuenci <154910381@qq.com>
2018-07-12 22:06:14 +0800

    读写硬盘属性文件

--- src/com/cier/io/Demo9.java
+++ src/com/cier/io/Demo9.java
@@ -1,11 +1,28 @@
 package com.cier.io;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
 
 public class Demo9 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        System.out.println(properties);
+        // 加载配置文件
+        properties.load(new FileInputStream("config.properties"));
+        System.out.println(properties);
+        // 在内存中修改，没有在硬盘上修改
+        properties.setProperty("tel","12322");
+        // 写回硬盘
+        properties.store(new FileOutputStream("config.properties"), "列表描述");
+        System.out.println(properties);
+
+    }
+
+    public static void demo2() {
         Properties properties = new Properties();
         properties.setProperty("name", "张三");
         properties.setProperty("tel", "1234567890");
