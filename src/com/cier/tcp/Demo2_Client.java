9cf4e44ba4874117ce14cda97f496967ce858d9a
liuenci <154910381@qq.com>
2018-07-15 15:21:02 +0800

    服务器端的多线程

--- src/com/cier/tcp/Demo2_Client.java
+++ src/com/cier/tcp/Demo2_Client.java
@@ -7,20 +7,8 @@ import java.net.Socket;
 public class Demo2_Client {
 
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException{
         Socket socket = new Socket("127.0.0.1",12345);
-        /*// 获取客户端输入流
-        InputStream inputStream = socket.getInputStream();
-        // 获取客户端输出流
-        OutputStream outputStream = socket.getOutputStream();
-
-        byte[] arr = new byte[1024];
-        int len = inputStream.read(arr);
-
-        System.out.println(new String(arr,0,len));
-        // 客户端向服务器写数据
-        outputStream.write("学习挖掘机哪家强".getBytes());*/
         // 将字节流包装成字符流
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         // PrintStream 中有写出换行的方法
@@ -29,8 +17,20 @@ public class Demo2_Client {
         System.out.println(bufferedReader.readLine());
         // 必须要有换行
         printStream.println("谢谢惠顾");
-
-
-
     }
+
+    /*public static void demo1() throws IOException {
+
+        Socket socket = new Socket("127.0.0.1",12345);
+        // 将字节流包装成字符流
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // PrintStream 中有写出换行的方法
+        PrintStream printStream = new PrintStream(socket.getOutputStream());
+
+        System.out.println(bufferedReader.readLine());
+        // 必须要有换行
+        printStream.println("谢谢惠顾");
+    }*/
+
+
 }
--- src/com/cier/tcp/Demo2_Server.java
+++ src/com/cier/tcp/Demo2_Server.java
@@ -8,18 +8,34 @@ public class Demo2_Server {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(12345);
+
+        while (true) {
+            // 接受客户端的请求
+            Socket socket = serverSocket.accept();
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        // 将字节流包装成字符流
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        // PrintStream 中有写出换行的方法
+                        PrintStream printStream = new PrintStream(socket.getOutputStream());
+
+                        printStream.println("欢迎光临");
+                        System.out.println(bufferedReader.readLine());
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+    }
+
+    public static void demo1() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12345);
         // 接受客户端的请求
         Socket socket = serverSocket.accept();
-        /*// 获取客户端输入流
-        InputStream inputStream = socket.getInputStream();
-        // 获取客户端输出流
-        OutputStream outputStream = socket.getOutputStream();
-        // 服务器向客户端写出数据
-        outputStream.write("百度一下，你就知道".getBytes());
-
-        byte[] arr = new byte[1024];
-        int len = inputStream.read(arr);*/
-
 
         // 将字节流包装成字符流
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -29,6 +45,5 @@ public class Demo2_Server {
         printStream.println("欢迎光临");
         System.out.println(bufferedReader.readLine());
         socket.close();
-
     }
 }
