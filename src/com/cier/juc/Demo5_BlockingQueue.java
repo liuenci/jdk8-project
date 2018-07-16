d46cd094fa095344762fc721214cc7c9f82e7677
liuenci <154910381@qq.com>
2018-07-16 11:32:24 +0800

    JUC 中的阻塞队列 BlockingQueue

---
+++ src/com/cier/juc/Demo5_BlockingQueue.java
@@ -0,0 +1,48 @@
+package com.cier.juc;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Demo5_BlockingQueue {
+    private static BlockingQueue<String> queue = new ArrayBlockingQueue<>(5);
+
+    /**
+     * 生产者
+     */
+    private static class Producer extends Thread {
+        @Override
+        public void run() {
+            try {
+                queue.put("product");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("prodece...");
+        }
+    }
+    private static class Consumer extends Thread {
+        @Override
+        public void run() {
+            try {
+                String product = queue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("consume...");
+        }
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            Producer producer = new Producer();
+            producer.start();
+        }
+        for (int i = 0; i < 5; i++) {
+            Consumer consumer = new Consumer();
+            consumer.start();
+        }
+        for (int i = 0; i < 3; i++) {
+            Producer producer = new Producer();
+            producer.start();
+        }
+    }
+}
