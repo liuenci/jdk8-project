7df0f1e8f1e116f10907705691e0b29d318c81f2
liuenci <154910381@qq.com>
2018-07-16 17:19:43 +0800

    线程本地存储 ThreadLocal

---
+++ src/com/cier/threadsafe/Demo4_ThreadLocal.java
@@ -0,0 +1,27 @@
+package com.cier.threadsafe;
+
+public class Demo4_ThreadLocal {
+
+    public static void main(String[] args) {
+        ThreadLocal threadLocal = new ThreadLocal();
+
+        Thread thread1 = new Thread(() -> {
+            threadLocal.set(1);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(threadLocal.get());
+            threadLocal.remove();
+        });
+
+        Thread thread2 = new Thread(() -> {
+            threadLocal.set(2);
+            threadLocal.remove();
+        });
+        
+        thread1.start();
+        thread2.start();
+    }
+}
