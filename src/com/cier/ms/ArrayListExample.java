5369b32d8e2084eb8f099e5a3f57911463ef07c4
liuenci <154910381@qq.com>
2018-08-01 15:39:26 +0800

    测试 addAll

--- src/com/cier/ms/ArrayListExample.java
+++ src/com/cier/ms/ArrayListExample.java
@@ -1,4 +1,22 @@
 package com.cier.ms;
 
+import java.util.ArrayList;
+
 public class ArrayListExample {
+
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(2);
+
+        ArrayList list = new ArrayList();
+        list.add("ss");
+        list.add("sg");
+
+        arrayList.addAll(list);
+
+        arrayList.forEach(System.out::println);
+    }
 }
--- src/com/cier/ms/Box.java
+++ src/com/cier/ms/Box.java
@@ -1,4 +1,11 @@
 package com.cier.ms;
 
-public class Box {
+public class Box<T> {
+    private T t;
+    public void set(T t){
+        this.t = t;
+    }
+    public T get(){
+        return t;
+    }
 }
